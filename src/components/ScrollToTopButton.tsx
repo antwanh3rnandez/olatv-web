4a0b8e322264d48610540c8f1933f5a7c7b6f181
Antwan Hernandez <115040575+antwanh3rnandez@users.noreply.github.com>
2023-11-11 01:47:43 -0600

    Actualizar ScrollToTopButton.tsx

---
+++ src/components/ScrollToTopButton.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { IconChevronUp } from '@tabler/icons-react';
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    setIsVisible(scrollY > 100);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <button className={`scroll-to-top ${isVisible ? 'visible' : 'hidden'}`} onClick={scrollToTop}>
+      <IconChevronUp size={24} />
+    </button>
+  );
+};
