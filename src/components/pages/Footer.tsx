f73b6b94c7bd6de74893e6c7a144bc820af79baf
Axol Dev <axol.dev@gmail.com>
2023-11-13 17:28:05 -0600

    fix bg footer

--- src/components/pages/Footer.tsx
+++ src/components/pages/Footer.tsx
@@ -63,13 +63,13 @@ export const Footer = () => {
             </p>
           </div>
         </div>
-        <div className='shadow-2xl my-6 flex flex-row items-center justify-center'>
+        <div className='shadow-2xl mt-6 flex flex-row items-center justify-center'>
             <div className="w-[25%] h-24 bg-redprimary" style={{ clipPath: 'polygon(0 100%, 100% 100%, 0% 0%)' }}>
             </div>
             <div className="w-[75%] h-24 bg-redprimary" style={{ clipPath: 'polygon(100% 100%, 0% 100%, 100% 0%)' }}>
             </div>
         </div>
-        <div className='footer z-50 w-full flex flex-col items-center justify-center' style={{fontFamily: 'Poppins'}}>
+        <div className='bg-background pt-6 w-full flex flex-col items-center justify-center' style={{fontFamily: 'Poppins'}}>
           <p className="mx-8 mt-4 mb-6 text-sm font-light text-center">
             DMCA: OLATV es un distribuidor externo y autorizado de servicios digitales de material de entretención que suministra en su totalidad VOD NETWORK, quienes cumplen con toda norma legal según el país de origen desde el cual distribuyen el servicio. VOD NETWORK no almacena ni sube en su sitio o servidor, cualquier archivo de vídeo, película, archivo multimedia de ningún tipo o cualquier otro material protegido por derechos de autor. VOD NETWORK y OLATV no poseen ni comercializan decodificadores de ningún tipo de TV satelital. OLATV cumple solo en calidad de distribuidor de servicio para el alcance nacional del territorio venezolano y de sudamerica.
           </p>
